16044ae703cb65aafd003e7158a6d202181b136a
Giovani Lima <giovani.lvc@gmail.com>
2021-07-03 23:08:50 -0300

    Adicionando arquivo do modulo 1 aula 4 - variaveis

---
+++ 01-logica-de-programacao/04-variaveis/variaveis.js
@@ -0,0 +1,26 @@
+//Tipo Number
+let a = 10;
+let b = 11.5;
+
+//Tipo String
+let c = 'Giovani'
+
+//Boolean
+let d = true;
+let e = false;
+
+//Undefined
+let f;
+let g = undefined;
+
+//function
+let h = function(){};
+let i = () => {};
+
+//object
+let j = {};
+let k = []; //array = vetor
+let l = null //Atribuição null, ou seja, vazio
+
+//como imprimir variaveis:
+console.log(c)
